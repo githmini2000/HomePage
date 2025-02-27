fb18f221f299faeda66102879f7f9624e1c873d1
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2025-02-27 18:35:25 +0530

    style: add Header styles

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,43 +5,40 @@ import { CiHeart } from "react-icons/ci";
 import { CiSearch } from "react-icons/ci";
 
 const Header = () => {
-    return (
-      <header className="bg-gray-100 p-4 flex items-center justify-between">
-       
-        <div className="flex items-center space-x-2">
-          <span className="text-lg font-medium">All Categories</span>
-          <SlArrowDown />
-        </div>
-  
-        <div className="flex items-center space-x-2 w-96">
-          <input
-            type="text"
-            placeholder="Search Product"
-            className="w-full py-2 px-4 border border-gray-300 rounded-md"
-          />
-          <CiSearch />
-        </div>
-  
-    
-        <div className="flex items-center space-x-4">
-          <div className="flex items-center space-x-1">
-            <IoPersonSharp size={24} />
-            <div>
-              <span className="text-lg">Welcome</span>
-              <br />
-              <span className="text-sm text-gray-500">Register/Sign in</span>
-            </div>
+  return (
+    <header className="bg-gray-200 p-4 flex items-center justify-between">
+      <div className="flex items-center space-x-5">
+        <span className="text-lg font-medium">All Categories</span>
+        <SlArrowDown />
+      </div>
+
+      <div className="relative flex items-center w-full max-w-3xl ">
+        <input
+          type="text"
+          placeholder="Search Product"
+          className="w-full py-2 px-4 pl-10 border  border-gray-300 rounded-full"
+        />
+        <CiSearch className="absolute right-3 text-gray-500" size={20} />
+      </div>
+
+      <div className="flex items-center space-x-4">
+        <div className="flex items-center space-x-1">
+          <IoPersonSharp size={35} />
+          <div>
+            <span className="text-sm text-gray-500">Welcome</span>
+            <br />
+            <span className="text-lg text-bold">Register/Sign in</span>
           </div>
-          <SlArrowDown />
         </div>
-  
-      
-        <div className="flex items-center space-x-6">
-          <PiHandbagSimple size={24} />
-          <CiHeart size={24} />
-        </div>
-      </header>
-    );
-  };
-  
-  export default Header;
+        <SlArrowDown />
+      </div>
+
+      <div className="flex items-center space-x-6">
+        <PiHandbagSimple size={24} />
+        <CiHeart size={28} />
+      </div>
+    </header>
+  );
+};
+
+export default Header;
