d487c738f985b96666a21c0112ce049cd7cbc3f6
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2025-02-27 15:32:30 +0530

    style: add footer styles

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,35 @@
-import { SlControlPlay } from "react-icons/sl";
+import { IoMdPlay } from "react-icons/io";
 
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white py-8">
+    <footer className="bg-gray-400 text-black text-xl font-medium py-8">
       <div className="max-w-7xl mx-auto px-6">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
           <div>
             <h3 className="text-xl font-bold underline mb-4">Quick Links</h3>
             <ul>
               <li className="flex items-center mb-3">
-                <SlControlPlay className="mr-2" /> Home
+                <IoMdPlay className="mr-2 " /> Home
               </li>
               <li className="flex items-center mb-3">
-                <SlControlPlay className="mr-2" /> About Us
+                <IoMdPlay className="mr-2  " /> About Us
               </li>
               <li className="flex items-center mb-3">
-                <SlControlPlay className="mr-2" /> Contact Us
+                <IoMdPlay className="mr-2" /> Contact Us
               </li>
               <li className="flex items-center mb-3">
-                <SlControlPlay className="mr-2" /> Terms & Conditions
+                <IoMdPlay className="mr-2" /> Terms & Conditions
               </li>
             </ul>
           </div>
 
-          <div>
-            <h3 className="text-xl font-bold underline mb-4">Quick Links</h3>
+          <div className="flex space-x-8 mt-10">
             <ul>
               <li className="flex items-center mb-3">
-                <SlControlPlay className="mr-2" /> FAQs
+                <IoMdPlay className="mr-2" /> FAQs
               </li>
               <li className="flex items-center mb-3">
-                <SlControlPlay className="mr-2" /> Privacy Policy
+                <IoMdPlay className="mr-2" /> Privacy Policy
               </li>
             </ul>
           </div>
@@ -40,7 +39,7 @@ const Footer = () => {
             <input
               type="email"
               placeholder="Your email address"
-              className="w-full p-2 mb-4 rounded-md border border-gray-300"
+              className="w-full p-2 mb-4 rounded-md border border-gray-300 bg-gray-300"
             />
             <button className="bg-green-900 text-white py-2 px-6 rounded-md hover:bg-green-700">
               Send
