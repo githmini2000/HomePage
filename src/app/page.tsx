3b25074a8ef270f8e9df663883237797c0269857
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2025-02-27 17:04:11 +0530

    refactor:remove Footer from page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import Homepage from "@/components/Homepage";
-import Footer from "@/components/Footer";
+
 export default function Home() {
   return (
     <main>
       <Homepage />
-      <Footer />
     </main>
   );
 }
