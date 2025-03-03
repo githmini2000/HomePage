aeb8321277399789adaf02e6ac7f2ae8dd84790f
GithminiSandupama <164888580+GithminiSandupama@users.noreply.github.com>
2025-03-04 00:12:14 +0530

    chore: update configuration

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,5 @@
 module.exports = {
   images: {
-    remotePatterns: [
-      {
-        protocol: 'http',
-        hostname: 'localhost',
-        port: '3001',
-        pathname: '/images/**',  
-      },
-    ],
+    domains: ['localhost'],  
   },
 };
